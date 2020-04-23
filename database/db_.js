6f6f75b202f4e69c3e3b1c5a975af5df03c96a11
Patrick Rottlaender <p.rottlaender@icloud.com>
2020-04-23 06:08:01 +0200

    database/db_.js file adapted. Commit database/db_.js

--- database/db_.js
+++ database/db_.js
@@ -1,15 +1,19 @@
 //database/db.js
 
+// load the mongoose module
 const mongoose = require('mongoose');
+
 mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-const mongodbpath = '<YOUR CONNECTION PATH>';
+const mongodbpath = 'YOUR-DB-USER:YOUR-DB-USER-PASSWD@localhost:27017/YOUR-DB-NAME';
 
 const startMongoDbServer = async function() {
   try {
 
-    await mongoose.connect(mongodbpath, { useFindAndModify: false })
+    await mongoose.connect(mongodbpath)
     .then(function() {
       console.log(`Mongoose connection open on ${mongodbpath}`);
     })
